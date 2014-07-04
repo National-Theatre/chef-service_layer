506700413470ffc942c852681e5599cee674226c
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-07-04 18:07:02 +0100

    add windows roles and features

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: service_layer
+# Attributes:: discovery
+#
+# Copyright 2014, National Theatre
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['windows']['webdeploy'] = 'http://download.microsoft.com/download/D/4/4/D446D154-2232-49A1-9D64-F5A9429913A4/WebDeploy_amd64_en-US.msi'
--- recipes/discovery.rb
+++ recipes/discovery.rb
@@ -7,6 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
+windows_package 'webdeploy' do
+  source node['windows']['webdeploy']
+  action :install
+end
+
+%w{ NetFx4 NetFx4ServerFeatures NetFx4Extended-ASPNET45 IIS-ASPNET45 IIS-NetFxExtensibility45 }.each do |feature|
+  windows_feature feature do
+    action :install
+  end
+end
+
+directory "Z:\\ServiceLayer.API_deploy" do
+  action :create
+  not_if { ::File.directory?("Z:\\ServiceLayer.API_deploy") }
+end
+
 #creates a new app pool
 iis_pool 'service_layer_pool' do
   runtime_version "4.0"
@@ -17,7 +33,11 @@ end
 iis_app "Default Web Site" do
   path "/ServiceLayer.API_deploy"
   application_pool "service_layer_pool"
-  physical_path "z:\ServiceLayer.API_deploy"
+  physical_path "Z:\\ServiceLayer.API_deploy"
   enabled_protocols "http"
   action :add
+end
+
+powershell_script "fix_iis_handler" do
+  code "Set-WebConfiguration //System.webServer/handlers -metadata overrideMode -value Allow -PSPath IIS:/"
 end
