9f0e1211f7b66c787f81791d9089ef073968af81
jpd4nt <jdrawneek@nationaltheatre.org.uk>
2014-07-05 13:46:41 +0100

    add powershell script to download code

--- recipes/discovery.rb
+++ recipes/discovery.rb
@@ -42,3 +42,32 @@ end
 powershell_script "fix_iis_handler" do
   code "Set-WebConfiguration //System.webServer/handlers -metadata overrideMode -value Allow -PSPath IIS:/"
 end
+
+powershell_script "get_code" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+Read-S3Object -BucketName ntdp-artifact -Key ServiceLayer.zip -File Z:\ServiceLayer.zip
+New-Item -ItemType Directory -Force -Path Z:\ServiceLayer
+
+$shell=new-object -com shell.application
+$location=$shell.namespace('Z:\ServiceLayer')
+ 
+# test the root folder now does exist, extract if it does.
+if($location){
+     $zipFiles = get-childitem Z:\ServiceLayer.zip
+     Write-Host "Number of ZIPs=$($zipFiles.count)" -fore yellow -back black
+ 
+     foreach($zipFile in $ZipFiles){
+          Write-Host "`t$($zipFile.fullname)" -fore green
+          $zipFolder = $shell.namespace($zipFile.fullname)
+          Write-Host "unzipping..." -fore green -back black
+          $location.Copyhere($zipFolder.items())
+     }
+ 
+}else{
+    Write-Host 'Oh Snap! - file path could not be created'
+}
+& Z:\ServiceLayer\ServiceLayer.API.deploy.cmd "/Y"
+  EOH
+  not_if { ::File.directory?("Z:\\ServiceLayer") }
+end
