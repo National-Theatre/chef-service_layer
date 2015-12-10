4a33e2f22fbd1ab4246ea02b2e2e296c4510df54
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2015-12-10 18:30:45 +0000

    Try to fix NewRelic agent

---
+++ recipes/fix_newrelic.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: service_layer
+# Recipe:: fix_newrelic
+#
+# Copyright 2015, National Theatre
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file 'C:\\NewRelicDotNetAgent_x64.msi' do
+  source 'https://download.newrelic.com/dot_net_agent/release/NewRelicDotNetAgent_x64.msi'
+  action :create
+end
+
+windows_package "Install New Relic .NET Agent" do
+#  action [:install]
+#  default_guard_interpreter :default
+  options "/qb NR_LICENSE_KEY=#{node['newrelic']['license']} INSTALLLEVEL=50"
+#  package_name "Install New Relic .NET Agent"
+  source "C:\\NewRelicDotNetAgent_x64.msi"
+#  timeout 600
+#  returns [0]
+#  declared_type :windows_package
+#  cookbook_name :newrelic
+#  recipe_name "dotnet_agent"
+  notifies :restart, "iis_site[Service Layer]", :delayed
+  notifies :restart, "iis_pool[service_layer_pool]", :delayed
+  not_if { ::File.directory?('C:\\Program Files\\New Relic\\.NET Agent') }
+end
