a81e0617330c1fa32ca74d8012428b4b49baa46c
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2016-05-04 12:27:58 +0100

    Try to get clocks in sync

--- recipes/sort_date.rb
+++ recipes/sort_date.rb
@@ -10,3 +10,24 @@
 batch "set_time_gmt" do
   code 'tzutil.exe /s "GMT Standard Time"'
 end
+
+batch "register_ntp" do
+  code 'w32tm /register'
+  action :nothing
+  notifies :run, 'batch[set_ntp]', :immediately
+end
+
+batch "set_ntp" do
+  code 'w32tm /config /manualpeerlist:"0.amazon.pool.ntp.org 1.amazon.pool.ntp.org uk.pool.ntp.org" /update'
+  action :nothing
+end
+
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Parameters" do
+  values [{
+    :name => "NtpServer",
+    :type => :string,
+    :data => '0.amazon.pool.ntp.org 1.amazon.pool.ntp.org uk.pool.ntp.org'
+  }]
+  action :create
+  notifies :run, 'batch[register_ntp]', :immediately
+end
