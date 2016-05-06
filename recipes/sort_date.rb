66bb94c81f56d7434db936ce2af6476ca1c8415c
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2016-05-06 01:41:15 +0100

    start ntp client service

--- recipes/sort_date.rb
+++ recipes/sort_date.rb
@@ -14,12 +14,20 @@ end
 batch "register_ntp" do
   code 'w32tm /register'
   action :nothing
-  notifies :run, 'batch[set_ntp]', :immediately
+  notifies :run, 'powershell_script[enable_ntp]', :immediately
 end
 
 batch "set_ntp" do
   code 'w32tm /config /manualpeerlist:"0.amazon.pool.ntp.org 1.amazon.pool.ntp.org uk.pool.ntp.org" /update'
   action :nothing
+end
+
+powershell_script 'enable_ntp' do
+  code <<-EOH
+  Start-Service w32time
+  EOH
+  action :nothing
+  notifies :run, 'batch[set_ntp]', :immediately
 end
 
 registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Parameters" do
